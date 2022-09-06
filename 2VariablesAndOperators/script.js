b1685651c8bd1db0afdc40ab62fccc2a980a8649
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-09-07 02:41:01 +0530

    Variables and operators

---
+++ 2VariablesAndOperators/script.js
@@ -0,0 +1,36 @@
+// //var(old/before es6), let, const
+// var digit1 = 22;
+// var x = 8;
+// var z = digit1+x;
+// alert(z);
+
+// let s = "string data";
+// s = "updated string data";
+// alert(s);
+
+// const k = 100; //can't be updated or changed..fixed value
+
+// let fname = 'Siddharth',  lname = 'Garg';
+// alert(fname+lname);
+
+// let variable; //declare
+// variable = 23; //assignment
+
+/*
+= assignment
+== comparison of value
+=== comparison of value and data type
++ add
+- sub
+* mul
+/ divide
+% modulo
+** power
++=, -=, *=, /=, %= etc.
+x++, y--
+operator precedency holds true here
+*/
+
+// let x = 5, y = 5;
+let x = 5, y = '5';
+console.log(x+y);
