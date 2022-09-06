685adaa25bebe7e399c25e53feb0cea33d347769
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-09-07 02:00:12 +0530

    helloworld and comments

---
+++ 1HelloWorldAndComments/script.js
@@ -0,0 +1,11 @@
+// alert("Hello World from script.js");
+
+// async creates error if we are doing dom manupulation and it has read js and executing it and dont know abiut that html tag but defer allows it to first read the document first and then executing so it knows the existence of that html tag
+
+document.getElementById("h").innerHTML = "Siddharth";
+
+console.log("Hello from console");
+
+/*var a;
+a = 5;
+alert(a);*/
