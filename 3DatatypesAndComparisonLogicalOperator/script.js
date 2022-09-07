f9a2657c558e2c6cf3555f62e3582c722b1173a3
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-09-07 13:31:17 +0530

    datatypes and comparison and logical operator

---
+++ 3DatatypesAndComparisonLogicalOperator/script.js
@@ -0,0 +1,36 @@
+//primitive - boolean, undefined, null, number, bigint, string, symbol
+
+let x = "hel\"lo"; //string
+
+let a = 22; //number
+let b = 3.14; //number
+
+let k = true; //boolean
+if (k){
+    console.log('True');
+};
+
+let arr = ['sid', 'boi', 'yo']; //array
+let obj = {name:'sid', sign:'yo'}; //object
+
+let variable; //undefined
+console.log(typeof(variable))
+
+let name = "Siddharth";
+alert(`hello ${name}`); //template literal... `` and ${}
+
+let var1 = 23;
+let var2 = "23";
+if (var1==var2){ //equal to
+    console.log("VAR1 AND VAR2 SAME value by ==")
+};
+if (var1===var2){ //equal value and equal type---first type and then value
+    console.log("VAR1 AND VAR2 NOT SAME BY ===")
+}
+// != not equal
+// !== not equal and not equal type
+// >, <, >=, <=
+// && logical and
+// || logical or
+// ! not
+
