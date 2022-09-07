ab182a7b188510a3f537465d79f598b54f0bc4e8
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-09-07 21:46:53 +0530

    basic loops and break, continue

---
+++ 6LoopsBreakContinue/script.js
@@ -0,0 +1,19 @@
+// let counter = 1;
+
+// while(counter<=10){
+//     document.write("WhileLoop ");
+//     i++;
+// } while loop
+
+
+// let count = 15;
+// do{
+//     document.write("DOWHILE ")
+// }while(count<=10); do while loop
+
+
+// for(let i=1; i<=10; i++){
+//     document.write("ForLoop");
+// } for loop
+
+//we can use nested loops and break and continue keywords too...usual.....
