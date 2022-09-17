8b6a7f167c7064490636a27bd9f9d1006b52c3d0
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-09-17 16:25:34 +0530

    Math Object

--- 13MathObject/script.js
+++ 13MathObject/script.js
@@ -10,3 +10,10 @@ console.log(Math.pow(2, 3));
 console.log(Math.sqrt(100));
 console.log(Math.min(100, -3, 22, 30, 23, -99, 0));
 console.log(Math.max(100, -3, 22, 30, 23, -99, 0));
+
+let rand1 = Math.random(); // between 0 and 1 exclusive
+let rand2 = Math.floor(Math.random()*10); //between 0-9 inclusive
+let rand3 = Math.floor(Math.random()*11); //between 0-10 inclusive
+let rand4 = Math.floor(Math.random()*100); //between 0-99 inclusive
+// can multily with (max-min) to get the random in range
+console.log(rand1);
