6b83e19dc1c7c2cae70876c56fb7bfb85f777929
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-09-17 16:13:17 +0530

    Math Object

---
+++ 13MathObject/script.js
@@ -0,0 +1,12 @@
+console.log(Math.PI); //pi
+console.log(Math.E); //eulers number
+
+let x = 4.7;
+console.log(Math.floor(x));
+console.log(Math.ceil(x));
+console.log(Math.round(x));
+console.log(Math.trunc(x));
+console.log(Math.pow(2, 3));
+console.log(Math.sqrt(100));
+console.log(Math.min(100, -3, 22, 30, 23, -99, 0));
+console.log(Math.max(100, -3, 22, 30, 23, -99, 0));
