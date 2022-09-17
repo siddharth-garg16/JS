73612de124aa9e7fcdf98fba64a01ed43d3af269
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-09-18 00:22:44 +0530

    Date Object

---
+++ 14DateObject/script.js
@@ -0,0 +1,8 @@
+let x = new Date(); //of our own computer
+let christmas = new Date(2022, 11, 25, 12, 00, 00);
+console.log(x);
+console.log(christmas);
+console.log(x.getDate());
+console.log(x.getMonth());
+console.log(x.getYear());
+console.log(x.getTime());
