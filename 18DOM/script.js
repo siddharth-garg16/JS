9d37e519656a0789c4a5566790e522c653bd5dc5
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-09-18 17:06:48 +0530

    DOM - selection by id, class and tag

---
+++ 18DOM/script.js
@@ -0,0 +1,13 @@
+// DOM is an API used to manipulate HTML documents.
+// when an html page is loaded, browser creates a tree like structure for the html tags and heirarchy..called DOM tree---document is the root of the DOM tree and then html and so on.....  
+/*
+JS can change all HTML elements and attributes in a page.
+JS can change all CSS styles in a page.
+JS can remove existing HTML elements and attributes in a page.
+JS can add new HTML elements and attributes in a page.
+JS can react to all HTML events in the page.
+JS can add new HTML events to the page.
+*/
+//In DOM, all HTML elements are objects i.e. both properties and methods.
+//--------------------------------------------------------------------------------------
+
