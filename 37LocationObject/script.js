9eb35ed71566e02d8cf48a6b5638015391255461
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-09-30 00:31:55 +0530

    location object in JS

---
+++ 37LocationObject/script.js
@@ -0,0 +1,17 @@
+//used to get information of the current page URL or redirecting etc...
+// useful for URLs...
+console.log(location.href);
+console.log(location.pathname);
+console.log(location.protocol);
+//can use multiple properties--can be used to set as well to redirect
+
+//redirect
+function fun(){
+    // window.location = "https://www.google.com"; --method1
+    // location.href = "https://www.google.com"; --method2
+    location.assign("https://www.google.com"); //--method3
+}
+
+location.replace("https://www.google.com"); //no back button to go to previous page
+
+
