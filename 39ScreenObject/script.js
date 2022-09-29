6ff473e496434ce0c66bf246c699fa29b44e22b6
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-09-30 00:56:22 +0530

    screen object in JS

---
+++ 39ScreenObject/script.js
@@ -0,0 +1,7 @@
+//information about current webpage or screen
+//window tells us about the browser but this tells about the actual screen
+console.log(screen.width);
+console.log(screen.height);
+//ie. if width and height of window and screen are same then user is viewing it in full screen
+console.log(screen.colorDepth);
+console.log(screen.orientation);
