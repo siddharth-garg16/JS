2f81b7c0916ad726df467050b4ddf89a8ece9efc
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-09-30 00:45:12 +0530

    navigator object in JS

---
+++ 38NavigatorObject/script.js
@@ -0,0 +1,16 @@
+//helps to find browser information and browser capabilities
+//helps to detect browser etc.......
+
+console.log(navigator.appName);
+console.log(navigator.appVersion);
+console.log(navigator.appCodeName);
+console.log(navigator.cookieEnabled);
+console.log(navigator.userAgent);
+console.log(navigator.platform);
+//etc.....
+//have methods like javaEnabled() etc...
+
+
+
+
+
