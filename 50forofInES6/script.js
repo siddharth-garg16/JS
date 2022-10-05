efc859e482d4dd92e1838ffb0a50d0b430723d55
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-10-05 16:57:16 +0530

    spread operator in es6

---
+++ 50forofInES6/script.js
@@ -0,0 +1,12 @@
+let arr = [2, 6, "lmao", 22, 30, 23, 11];
+
+for(let ele of arr){
+    console.log(ele);
+}
+
+
+// can be used in strings too
+let mystr = "SID";
+for(let char of mystr){
+    console.log(char);
+}
