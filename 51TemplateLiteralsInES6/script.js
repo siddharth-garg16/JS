4386d0ecd0061b397e9c9a81a2271561d4ee081f
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-10-05 17:23:45 +0530

    template literals in es6

---
+++ 51TemplateLiteralsInES6/script.js
@@ -0,0 +1,8 @@
+let str1 = "hello \n world";
+let str2 = `hello 
+world`;
+console.log(str1);
+console.log(str2); //will print literally how you have written in the code editor
+
+let name = "Siddharth";
+console.log(`My name is ${name}.`); //easier to use than concatenate...like f string in python
