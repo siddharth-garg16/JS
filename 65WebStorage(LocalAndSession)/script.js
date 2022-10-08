c2169d16198919f844d31f793c3c19af5b232d9f
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-10-08 15:59:15 +0530

    web storage(local and session)

---
+++ 65WebStorage(LocalAndSession)/script.js
@@ -0,0 +1,11 @@
+//read important
+
+//local storage
+localStorage.setItem("firstName", "Siddharth"); //set
+console.log(localStorage.getItem("firstName")); //get
+localStorage.removeItem("firstName"); //remove
+
+//session storage
+sessionStorage.setItem("firstName", "Siddharth"); //set
+console.log(sessionStorage.getItem("firstName")); //get
+localStorage.removeItem("firstName"); //remove
