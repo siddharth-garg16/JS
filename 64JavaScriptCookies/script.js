88d1f05095fa55dbbcc8b81ac0850cfcce3752e9
Siddharth Garg <siddharthgarg16082000@gmail.com>
2022-10-08 14:56:21 +0530

    cookies in JS

---
+++ 64JavaScriptCookies/script.js
@@ -0,0 +1,6 @@
+document.cookie("item=medicine: expires=Sun, 9 Oct 2022 12:00:00 UTC");
+//parameters - expires, domain, path, secure, name=value
+let x = document.cookie;
+alert(x);
+//delete by using past date
+
